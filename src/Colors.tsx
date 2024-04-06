ff43850d083e77bc4e525da1fb717d1c9102ad05
juanRCoder <guillecarck@gmail.com>
2024-04-05 22:20:16 -0500

    feat:add event for change colors

--- src/Colors.tsx
+++ src/Colors.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 function Colors() {
     const [datos, setDatos]: any = useState(null);
+    const [changeColor, setChangeColor] = useState(false);
 
     useEffect(() => {
         const request = async () => {
@@ -9,36 +10,65 @@ function Colors() {
                 const res = await fetch('https://perfect-tiara-yak.cyclic.app/colors');
                 const data = await res.json();
                 setDatos(data);
-                console.log(data);
             }
             catch (e) {
                 console.error('Error al consumir API', e);
             }
         }
         request();
-    }, [])
+    }, [changeColor])
 
     const RGBtoHex = (rgb: string) => {
-        //Convierte, secciona la cadena rgb y pasa a un array de numeros con los colores rgb.
         const arrColors = rgb.slice(4, -1).split(',').map(Number);
-        //Itera sobre los colores rgb y los vuelve hexadecimal.
         return `#${arrColors.map((c) => c.toString(16).padStart(2, '0')).join('')}`
     }
 
-    return (
-        <div>
-            {datos && datos.map((color: { [key: string]: string }, i: number) => {
-                const name = Object.keys(color)[0];
-                const value = color[name];
-                const hexaColor = RGBtoHex(value);
+    const copyHexaColor = async (hexaColor: string) => {
+        try {
+            // Copia el color hexadecimal al portapapeles utilizando API clipboard
+            await navigator.clipboard.writeText(hexaColor);
+            alert(`Color copiado: ${hexaColor}`);
+            console.log(`Color copiado: ${hexaColor}`);
+        } catch (error) {
+            console.error('Error clipboard:', error);
+        }
+    }
 
-                return <div key={i}>
-                    <span style={{ backgroundColor: hexaColor, padding: '10px' }}>
-                    </span>
-                    <p style={{ padding: '10px' }}>{hexaColor}</p>
-                </div>;
-            })}
-        </div>
+    return (
+        <>
+            <section
+                className="w-screen"
+            >
+                <h1>Random Colors</h1>
+                {datos && datos.map((color: { [key: string]: string }, i: number) => {
+                    const name = Object.keys(color)[0];
+                    const value = color[name];
+                    const hexaColor = RGBtoHex(value);
+
+                    return (
+                        <div key={i}
+                            className="p-8"
+                            style={{ backgroundColor: hexaColor }}
+                        >
+                            <p
+                                className="cursor-pointer"
+                                onClick={() => copyHexaColor(hexaColor)}
+                            >
+                                {hexaColor}
+                            </p>
+                        </div>)
+                })}
+            </section>
+            <div className="flex outline mt-5 justify-center">
+                <button
+                    className="bg-gray-900 text-slate-300 p-2 pl-3 pr-3 rounded"
+                    onClick={() => setChangeColor(!changeColor)}
+                >
+                    Update Colors
+                </button>
+            </div>
+
+        </>
     )
 }
 
