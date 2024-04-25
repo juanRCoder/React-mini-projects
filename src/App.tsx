c626c5c0f67828a35c99f7e3b7ec9e5a566a85e5
juanRCoder <guillecarck@gmail.com>
2024-04-24 20:08:06 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Principal from './Principal/Principal';
 import ColorPallet from './colorPallet/ColorPallet';
-// import TipAmount from './TipAmount'
 import './App.css';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Principal />} />
-          {/* <Route path="/" element={<TipAmount />} /> */}
           {/* //Proyectos */}
           <Route path="/color-pallet" element={<ColorPallet />} />
         </Routes>
