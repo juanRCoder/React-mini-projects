e890cc68c79d8b89d7691795b319186b67860654
juanRCoder <guillecarck@gmail.com>
2024-05-13 12:57:28 -0500

    feat: add new project 'Calculator'

--- src/Calculator/Calculator.tsx
+++ src/Calculator/Calculator.tsx
@@ -1,6 +1,71 @@
+import { useState } from 'react';
+import './Calculator.scss';
+
 function Calculator() {
+    const [number, setNumber] = useState<string>('');
+    const [valuePrev, setValuePrev] = useState<string>('');
+
+    const addValue = (n: string) => {
+        setNumber(prev => prev += n);
+    }
+    const minusValue = (n: string) => {
+        const newValue: string = n.toString().slice(0, -1);
+        setNumber(newValue);
+    }
+    const operar = (operation: string) => {
+        const resto = operation.split('').filter(x => x == '%').join('');
+        if (resto) {
+            const valor = operation.split(/\+|-|\*|\//)[0];
+            operation = operation.replace(resto, `*${valor}/100`);
+        }
+        console.log(operation);
+        const result = eval(operation);
+        setNumber(result);
+        setValuePrev(`${operation}=`);
+    }
+
+    const reset = () => {
+        setNumber('');
+        setValuePrev('');
+    }
     return (
-        <div>Calculator</div>
+        <main className="vs:items-center vs:justify-center vs:flex-col vs:flex vs:bg-emerald-200 bg- font-InriaSans h-screen bg-emerald-900">
+            <h1 className='vs:pt-1 vs:text-emerald-900 text-green-200 text-center py-6 text-5xl'>Calculator 5.0</h1>
+            <section className='lg:w-4/12 vs:rounded-2xl vs:bg-emerald-900 p-4 pt-0'>
+                <div className='vs:mb-2 mb-5 py-5'>
+                    <p className='font-bold text-md pt-2 text-emerald-600 rounded-t-xl bg-emerald-200 text-lg h-10 text-right pr-4'>{valuePrev}</p>
+                    <input type='text' defaultValue={number}
+                        className='vs:text-4xl font-bold text-5xl text-right pb-2 pr-4 border-none w-full text-emerald-800 bg-emerald-200 rounded-b-xl'
+                    />
+                </div>
+                <div className="gap-1 grid grid-cols-4">
+                    <button className='hover:bg-emerald-700 bg-emerald-800' onClick={() => addValue('%')}>%</button>
+                    <button className='hover:bg-emerald-700 bg-emerald-800' onClick={() => addValue('/')}>/</button>
+                    <button className='hover:bg-emerald-700 bg-emerald-800' onClick={() => minusValue(number)}>CE</button>
+                    <button className='hover:bg-emerald-700 bg-emerald-800' onClick={() => reset()}>C</button>
+
+                    <button className='hover:bg-emerald-500 bg-emerald-600' onClick={() => addValue('7')}>7</button>
+                    <button className='hover:bg-emerald-500 bg-emerald-600' onClick={() => addValue('8')}>8</button>
+                    <button className='hover:bg-emerald-500 bg-emerald-600' onClick={() => addValue('9')}>9</button>
+                    <button className='hover:bg-emerald-700 bg-emerald-800' onClick={() => addValue('*')}>X</button>
+
+                    <button className='hover:bg-emerald-500 bg-emerald-600' onClick={() => addValue('4')}>4</button>
+                    <button className='hover:bg-emerald-500 bg-emerald-600' onClick={() => addValue('5')}>5</button>
+                    <button className='hover:bg-emerald-500 bg-emerald-600' onClick={() => addValue('6')}>6</button>
+                    <button className='hover:bg-emerald-700 bg-emerald-800' onClick={() => addValue('-')}>-</button>
+
+                    <button className='hover:bg-emerald-500 bg-emerald-600' onClick={() => addValue('1')}>1</button>
+                    <button className='hover:bg-emerald-500 bg-emerald-600' onClick={() => addValue('2')}>2</button>
+                    <button className='hover:bg-emerald-500 bg-emerald-600' onClick={() => addValue('3')}>3</button>
+                    <button className='hover:bg-emerald-700 bg-emerald-800' onClick={() => addValue('+')}>+</button>
+
+                    <button className='hover:bg-emerald-500 bg-emerald-600' onClick={() => addValue('-')}>+/-</button>
+                    <button className='hover:bg-emerald-500 bg-emerald-600' onClick={() => addValue('0')}>0</button>
+                    <button className='hover:bg-emerald-500 bg-emerald-600' onClick={() => addValue('.')}>.</button>
+                    <button className='hover:bg-red-300 bg-red-400' onClick={() => operar(number)}>=</button>
+                </div>
+            </section>
+        </main>
     )
 }
 
--- src/Principal/Principal.tsx
+++ src/Principal/Principal.tsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 import { FaReact } from "react-icons/fa";
 import ColorPallet from '../assets/collorPallet.webp';
 import TipAmount from '../assets/tipAmountApp.webp';
-import './Principal.scss';
 import PwsdGenerator from '../assets/pswd-generator.webp';
+import Calculator from '../assets/calculator.webp';
 
 function Principal() {
     return (
@@ -14,16 +14,16 @@ function Principal() {
                 <h1 className='vm:text-4xl vs:text-3xl text-center vs:px-5 px-2 text-slate-100 font-semibold text-22'>React-mini-projects</h1>
                 <FaReact className='text-sky-400 w-12 h-12'/>
             </div>
-            <section className='gap-5 mx-5 vs:grid-cols-3 vs:grid'>
+            <section className='mt-8 gap-5 mx-5 lg:grid-cols-4 md:grid-cols-3 vs:grid-cols-2 vs:grid'>
                 {[
-                    ['Collor Pallet', '/color-pallet', ColorPallet, ],
-                    ['Tip Amount App', '/tip-amount', TipAmount],
-                    ['Pswd Generator', '/pswd-generator', PwsdGenerator],
-                    // ['Calculator', '/calculator', PwsdGenerator],
-                ].map(([text, path, icon], i) => (
-                    <Link key={i} to={path} className={`mt- stext-white bg-teal-100/25 shadow-inner font-bold no-underline my-8 transition hover:scale-105 cursor-pointer rounded-xl py-2 items-center flex flex-col`}>
+                    ['Collor Pallet', '/color-pallet', ColorPallet, 'bg-slate-200/25'],
+                    ['Tip Amount App', '/tip-amount', TipAmount, 'bg-cyan-100/25'],
+                    ['Pswd Generator', '/pswd-generator', PwsdGenerator, 'bg-pink-700/25'],
+                    ['Calculator', '/calculator', Calculator, 'bg-emerald-200/25'],
+                ].map(([text, path, icon, bg], i) => (
+                    <Link key={i} to={path} className={`mt-4 vs:mt-0 stext-white ${bg} shadow-inner font-bold no-underline transition hover:scale-105 cursor-pointer rounded-xl py-2 items-center flex flex-col`}>
                         <img className='vm:w-20 vm:h-20 h-24 w-24' src={icon} />
-                        <p className="text-white vm:text-lg vs:text-lg font-semibold text-2xl text-center w-full">{text}</p>
+                        <p className="text-white pt-1 vs:text-lg font-semibold text-2xl text-center w-full">{text}</p>
                     </Link>
                 ))}
             </section>
